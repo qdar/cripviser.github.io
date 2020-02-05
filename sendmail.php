3e8d56c36000fc9cb6870000cab6d318b8fc4c90
qdar <59022994+qdar@users.noreply.github.com>
2020-02-05 22:50:03 +0300

    04

--- sendmail.php
+++ sendmail.php
@@ -1,12 +1,13 @@
 <?php
 	header('Content-Type: text/html; charset=utf-8', true);
-	if($_POST['user_company'] != '' || $_POST['user_phone'] != '') {
+	if($_POST['user_company'] != '' || $_POST['user_mail'] != '') {
 		$headers = "Content-type: text/html; charset=utf-8\r\n";
 		$subject = "Order from Apelsin logistic";
 		$name = "Name: ".$_POST['user_company'];
-		$phone = "Contact: ".$_POST['user_phone'];
+		$email = "Email: ".$_POST['user_mail'];
+		$msg = "Message: ".$_POST['user_message'];
 
-		mail("webraduke@gmail.com", $subject, $name."\n\r<br/>".$phone, $headers);
+		mail("webraduke@gmail.com", $subject, $name."\n\r<br/>".$email."\n\r<br/>".$msg, $headers);
 	}
 	else {
 		$msg_error = "Sorry, something went wrong. Please try again later.";
